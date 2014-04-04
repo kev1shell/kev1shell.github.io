0bc7e0316f951035b7749e0b5aff993cf9bc00d4
kev1shell <kev1shell@neo.tamu.edu>
2014-04-04 11:23:20 -0500

    added objectGlobals.js

---
+++ javascript/objectGlobals.js
@@ -0,0 +1,42 @@
+//Global variables used by villageWars objects
+
+//Unit/Structure costs"
+var villagerFoodCost = 4;
+var villagerTimberCost = 0;
+var villagerStoneCost = 0;
+var villagerFoodUpKeep = 2;
+
+var warriorFoodCost = 4;
+var warriorTimberCost = 2;
+var warriorStoneCost = 1;
+var warriorFoodUpKeep = 4;
+
+var farmFoodCost = 0;
+var farmTimberCost = 2;
+var farmStoneCost = 0;
+
+var villageFoodCost = 0;
+var villageTimberCost = 8;
+var villageStoneCost = 4;
+
+//base unit stats
+var villagerMaxMovementPoints = 4;
+var villagerMaxHealth = 1;
+var villagerDefense = 0;
+var villagerAttack = 0;
+
+var warriorMaxMovementPoints = 2;
+var warriorMaxHealth = 10;
+var warriorDefense = 2;
+var warriorAttack = 5;
+
+//Resource Gathering Rates:
+var farmCollectionRate = 4;
+var timberCollectionRate = 2;
+var stoneCollectionRate = 1;
+
+//max village number:
+var maxVillages = 3;
+
+//error string
+var error = "";
