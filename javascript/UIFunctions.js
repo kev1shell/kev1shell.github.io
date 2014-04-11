bef7c26db66a8a1a31666566725629ada7da255c
kev1shell <kev1shell@neo.tamu.edu>
2014-04-11 15:47:53 -0500

    still playing with buttons

--- javascript/UIFunctions.js
+++ javascript/UIFunctions.js
@@ -278,7 +278,7 @@ function displayEndTurnElement(color)
 	}
 	
 	var ETsquare = new createjs.Shape();
-	ETsquare.graphics.beginFill(color).drawRoundRect(0, 0, 100, 43);
+	ETsquare.graphics.beginFill(color).drawRoundRect(0, 0, 100, 43,2);
 	ETsquare.x = 740;
 	ETsquare.y = 0;
 	ETsquare.name = "ETsquare";
