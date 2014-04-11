5b8b43291c78c7f7869b64ba2eeaa09bb4a6fe83
kev1shell <kev1shell@neo.tamu.edu>
2014-04-11 18:20:49 -0500

    adjusting button text y position

--- javascript/button.js
+++ javascript/button.js
@@ -43,7 +43,7 @@ var Button = function(_name, _x, _y,_width,_height)
 		var fontSize = Math.floor(this.height - 2*10);
 		this.textShape = new createjs.Text(this.text, "bold "+fontSize+"px "+this.fontType, this.fontColor);
 		this.textShape.x = this.x + this.width/2 - this.textShape.getBounds().width/2;
-		this.textShape.y = this.y + this.height/2 - this.textShape.getBounds().height/2;
+		this.textShape.y = 1+this.y + this.height/2 - this.textShape.getBounds().height/2;
 		this.textShape.name = this.name+"-Text";
 		stage.addChild(this.textShape);
 		
