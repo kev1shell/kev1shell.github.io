6743b0e7cd16efcae28622d113733cafe2d3cf5e
kev1shell <kev1shell@neo.tamu.edu>
2014-04-17 23:36:28 -0500

    l

--- javascript/animations.js
+++ javascript/animations.js
@@ -39,11 +39,11 @@ function snow()
 	//release snow variable
 	snowing = true;
 	
-	for(var i=0;i<stage.children.length;i++)
+	/* for(var i=0;i<stage.children.length;i++)
 	{
 		var shape = stage.getChildAt(i);
 		shape.cache(-6, -6, 6 * 2, 6 * 2);
-	}
+	} */
 	
 	//generate snow particles
 	/* for(var i=0;i<numParticles;i++)
