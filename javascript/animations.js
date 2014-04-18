71a3518899805de8b5306aec863591d6a2748f68
kev1shell <kev1shell@neo.tamu.edu>
2014-04-17 21:08:28 -0500

    added animations.js

---
+++ javascript/animations.js
@@ -0,0 +1,49 @@
+var snowing = false;
+var particles = [];
+var refreshPeriod = 42;
+
+function stopSnow()
+{
+	snowing = false;
+}
+
+function snow()
+{
+	var numParticles = 20;
+	particles = [];
+	
+	//release snow variable
+	snowing = true;
+	
+	//generate snow particles
+	for(var i=0;i<numParticles;i++)
+	{
+		//particle radius
+		var radius = Math.floor(Math.random()*5) + 5;
+		//particle shape
+		var particle = new createjs.Shape();
+		particle.graphics.beginFill("white").drawCircle(0, 0, radius);
+		particle.x = Math.floor(Math.random()*840);;
+		particle.y = 50;
+		particle.name = "particle"+i;
+		stage.addChild(particle);
+		particles.push(particle);
+	}
+	
+	animateSnow(particles);
+}
+
+function animateSnow(particles)
+{
+	var fallRate = 1;
+	
+	while(snowing == true)
+	{
+		for(var i=0;i<particles.length;i++)
+		{
+			particles[i].y = particles[i].y - fallRate;
+		}
+		stage.update();
+		window.setTimeout(animateSnow, refreshPeriod);
+	}
+}
