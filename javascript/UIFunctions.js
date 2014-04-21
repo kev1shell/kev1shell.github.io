1c4702b234046624262155eb4e4cc591cfdf9394
kev1shell <kev1shell@neo.tamu.edu>
2014-04-21 10:22:49 -0500

    l

--- javascript/UIFunctions.js
+++ javascript/UIFunctions.js
@@ -180,7 +180,7 @@ function displayVictoryScreen()
 {
 	//some text
 	var titalText = new createjs.Text("Victory!", "bold 54px Arial", "black");
-	titalText.x = 300;
+	titalText.x = 310;
 	titalText.y = 150;
 	titalText.name = "titalText";
 	stage.addChild(titalText);
